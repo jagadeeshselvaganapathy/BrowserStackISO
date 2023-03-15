8f583e64093a2dc2aeed9a8ef797e1abefa7a0e8
jagadeesh <jagadeeshpragadeesh@gmail.com>
2023-03-15 14:47:59 +0530

    ios script

--- src/test/java/com/employee/CameraAccessTest.java
+++ src/test/java/com/employee/CameraAccessTest.java
@@ -9,62 +9,85 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import com.browserstack.config.Platform;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class CameraAccessTest {
-	
-@Test
-private void empId() throws MalformedURLException, InterruptedException {
 
+	@Test
+	private void empId() throws MalformedURLException, InterruptedException {
 
 		DesiredCapabilities caps = new DesiredCapabilities();
-
-		// Set your access credentials
-		caps.setCapability("browserstack.user", "jagadeesh_fKpl7b");
-		caps.setCapability("browserstack.key", "JybmMjv9aYXwps7TMbpC");
-
-       // Set URL of the application under test
-		caps.setCapability("app", "bs://e1e0b3b2426ca7d44371c959b979ee1f2015e43f");
+		caps.setCapability("browserstack.user", "jagadeesh_Bc7WMr");
+		caps.setCapability("browserstack.key", "RAhWbcP7fxDPHvSqLT7t");
+        caps.setCapability("app", "bs://444bd0308813ae0dc236f8cd461c02d3afa7901d");
     	
-
-       // Specify device and os_version for testing
-		caps.setCapability("device", "Google Pixel 3");
-    	caps.setCapability("os_version", "9.0");
-
-       // Set other BrowserStack capabilities
+	    caps.setCapability("device", "iPhone XS");
+        caps.setCapability("os_version", "12");
 		caps.setCapability("project", "First Java Project");
 		caps.setCapability("build", "browserstack-build-1");
 		caps.setCapability("name", "first_test");
 		caps.setCapability("autoGrantPermissions", true);
+		caps.setCapability("browserstack.debug", "true");
+		caps.setCapability("browserstack.local", "false");
 
-		// Initialise the remote Webdriver using BrowserStack remote URL
-		// and desired capabilities defined above
+		
 		WebDriver driver = new RemoteWebDriver(new URL("http://hub-cloud.browserstack.com/wd/hub"), caps);
 		Thread.sleep(15000);
-//		   driver.get("calculator_app_url");
-		   Thread.sleep(5000);
-		      driver.findElement(By.id("btn1")).click();
-		      Thread.sleep(5000);
-		      driver.findElement(By.id("btn2")).click();
-		      Thread.sleep(5000);
-		      driver.findElement(By.id("btn3")).click();
-		      Thread.sleep(5000);
-		      driver.findElement(By.id("btn4")).click();
-		      
-		      // Get the result and print it
-//		      String result = driver.findElement(By.id("result")).getText();
-//		      System.out.println("Result: " + result);
 
-//		boolean contains = driver.getCurrentUrl().contains("xyz");
-//		Assert.assertTrue(contains);
-	
-		
-	
-
-		// Quit the driver
+		driver.findElement(By.id("Text Button")).click();
+	       Thread.sleep(5000);
+	        driver.findElement(By.id("Text Input")).sendKeys("hello@browserstack.com");;
+	        Thread.sleep(5000);
+	        WebElement element = driver.findElement(By.id("Text Output"));
+	        if(element != null && element.getText().equals("hello@browserstack.com"))
+	            assert(true);
+	        else
+	            assert(false); 
 		driver.quit();
 	}
+
+	@Test
+	private void empId2() throws MalformedURLException, InterruptedException {
+
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("browserstack.user", "jagadeesh_Bc7WMr");
+    	caps.setCapability("browserstack.key", "RAhWbcP7fxDPHvSqLT7t");
+    	
+    	// Set URL of the application under test
+    	caps.setCapability("app", "bs://444bd0308813ae0dc236f8cd461c02d3afa7901d");
+    	
+    	// Specify device and os_version for testing
+	    caps.setCapability("device", "iPhone 6S");
+        caps.setCapability("os_version", "12");
+    
+        
+    	// Set other BrowserStack capabilities
+    	caps.setCapability("project", "First Java Project");
+    	caps.setCapability("build", "browserstack-build-1");
+    	caps.setCapability("name", "first_test");
+		caps.setCapability("browserstack.debug", "true");
+		caps.setCapability("browserstack.interactiveDebugging", "true");
+
+		
+		WebDriver driver = new RemoteWebDriver(new URL("http://hub-cloud.browserstack.com/wd/hub"), caps);
+		Thread.sleep(15000);
+
+		driver.findElement(By.id("Text Button")).click();
+	       Thread.sleep(5000);
+	        driver.findElement(By.id("Text Input")).sendKeys("hello@browserstack.com");;
+	        Thread.sleep(5000);
+	        WebElement element = driver.findElement(By.id("Text Output"));
+	        if(element != null && element.getText().equals("hello@browserstack.com"))
+	            assert(true);
+	        else
+	            assert(false); 
+		driver.quit();
+	}
+
 }
